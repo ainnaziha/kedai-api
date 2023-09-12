0f1b2a945083e58bbd6bea0edd7beb2d976bbb01
jian <jian@DESKTOP-A03CMV3>
2023-09-12 13:26:06 +0800

    add models

---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KedaiAPI.Models
+{
+    public class Login
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace KedaiAPI.Models
+{
+    public class Response
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
