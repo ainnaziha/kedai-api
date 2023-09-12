15a822035bcdd52f099517b11470020088113c85
Ain Naziha <100841335+ainnaziha@users.noreply.github.com>
2023-09-12 13:27:20 +0800

    feature: database and models (#1)
    * initial migrations
    
    * chore: update readme
    
    * add models

---
+++ Models/Register.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KedaiAPI.Models
+{
+    public class Register
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+    }
+}
