d2fa633086151014f6ce3f5e6f22f45dbeca6929
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-15 04:05:11 +0800

    feat: create cart response class

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -6,7 +6,7 @@
         public int CartId { get; set; }
         public int ProductId { get; set; }
         public int Quantities { get; set; }
-        public bool isDeleted   { get; set; }
+        public bool IsDeleted   { get; set; }
 
         public Cart Cart { get; set; }
         public Product Product { get; set; }
---
+++ Models/CartResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace KedaiAPI.Models
+{
+    public class CartItemResponse
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double Subtotal { get; set; }
+    }
+
+    public class CartResponse
+    {
+        public int Id { get; set; }
+        public double Total { get; set; }
+        public List<CartItemResponse> Items { get; set; }
+    }
+}
