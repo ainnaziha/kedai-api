5ddfc707b8fe89d6e9cb562159f1aa6de5e034e2
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-15 02:32:40 +0800

    fix: cart and cartItem model

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -3,15 +3,9 @@
     public class Cart
     {
         public int Id { get; set; }
-        public int CartId { get; set; }
-        public int Quantities { get; set; }
-        public bool isDeleted   { get; set; }
-        public int ProductId { get; set; }
         public string UserId { get; set; }
+        public bool IsCompleted { get; set; }
 
-
-        public virtual Product Product { get; set; }
-        public virtual User User { get; set; }
-
+        public ICollection<CartItem> CartItems { get; } = new List<CartItem>();
     }
 }
