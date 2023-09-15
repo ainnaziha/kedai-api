0e640292ab6681bb8041acdc2d2692a13939f604
muza <muzallyefah@gmail.com>
2023-09-15 14:35:57 +0800

    feat: add order request model

---
+++ Models/OrderRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace KedaiAPI.Models
+{
+    public class OrderNoRequest
+    {
+        public int CartId {  get; set; }
+        public string Total { get; set; }
+    }
+
+    public class CompleteOrderRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        //todo: check if stripe return invoice no, if yes add here else remove comment
+    }
+}
