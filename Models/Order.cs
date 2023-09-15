d5a7914cdd94c5e488afb7eacceb89824f5e4874
muza <muzallyefah@gmail.com>
2023-09-15 14:34:54 +0800

    feat: add order model

---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace KedaiAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public string UserId { get; set; }
+        public string OrderNo { get; set; }
+        public string Total { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
