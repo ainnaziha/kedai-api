cc21bd65f9a6d18cae5a83ef9bfaef428ddbdfa1
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-17 17:59:06 +0800

    chore: create orders table and refactor carts

---
+++ Migrations/20230917095404_createOrdersTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KedaiAPI.Migrations
+{
+    public partial class CreateOrdersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    OrderNo = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    InvoiceNo = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Total = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Town = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Orders");
+        }
+    }
+}
