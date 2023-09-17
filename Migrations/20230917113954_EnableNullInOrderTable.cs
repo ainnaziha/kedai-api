fd58056366e8916dfcd364a195a5fdc4a1f7406a
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-17 19:42:35 +0800

    fix: unable to checkout

---
+++ Migrations/20230917113954_EnableNullInOrderTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KedaiAPI.Migrations
+{
+    public partial class EnableNullInOrderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Total",
+                table: "Orders",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true); 
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
