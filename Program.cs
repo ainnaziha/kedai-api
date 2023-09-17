8e4ddb3c9503d226fb35266fd6c1064f51de6386
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-17 23:36:41 +0800

    fix: unable to open website

--- Program.cs
+++ Program.cs
@@ -92,18 +92,10 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-    app.UseStaticFiles(new StaticFileOptions
-    {
-        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
-        RequestPath = "/uploads"
-    });
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
