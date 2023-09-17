94ce1ce6a80cacac8b2d76e98cb7a4f9d51996ec
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-17 19:15:38 +0800

    chore: add order id to cart

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string UserId { get; set; }
         public int ProductId { get; set; }
+        public int OrderId { get; set; }
         public int Quantity { get; set; }
         public bool IsDeleted { get; set; }
 
