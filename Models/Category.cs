07d03fa4d231bd38619285dd924190c1aa43ff69
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-21 00:15:49 +0800

    fix: include category on product response (#19)

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,14 +2,7 @@
 {
     public class Category
     {
-        public Category()
-        {
-            Products = new HashSet<Product>();
-        }
-
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-
-        public virtual ICollection<Product> Products { get; set; }
     }
 }
